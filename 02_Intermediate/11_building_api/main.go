644359a3e8d60678ed15e93b138b86e2760b6017
Atharva Naik <atharvan.coder@gmail.com>
2026-02-22 17:14:03 +0530

    added basic GET and POST routes

---
+++ 02_Intermediate/11_building_api/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Course struct {
+	Title      string  `json:"title"`
+	Instructor string  `json:"instructor"`
+	Price      float64 `json:"price"`
+}
+
+// Global "Database" to store courses
+var courses []Course
+
+// 1. GET Request Handler
+func getCourseHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// If the database is empty, let's load a default one!
+	if len(courses) == 0 {
+		courses = append(courses, Course{Title: "Golang Basics", Instructor: "Atharva", Price: 99.6})
+	}
+
+	json.NewEncoder(w).Encode(courses)
+}
+
+// 2. POST Request Handler (Reading incoming JSON from Client)
+func createCourseHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse the JSON data from the Request Body!
+	var newCourse Course
+	err := json.NewDecoder(r.Body).Decode(&newCourse)
+
+	if err != nil {
+		// If they send bad JSON, send a 400 Bad Request error!
+		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
+		return
+	}
+
+	// Save it to our text database
+	courses = append(courses, newCourse)
+
+	// Return a 201 Created Status
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "Course '%s' successfully created!", newCourse.Title)
+}
+func main() {
+	// 3. Routing HTTP Methods (A massive new feature in Go 1.22+)
+	// You just put the HTTP Method right before the path!
+	http.HandleFunc("GET /courses", getCourseHandler)
+	http.HandleFunc("POST /courses", createCourseHandler)
+
+	fmt.Println("Server running on port 8080. Try:")
+	fmt.Println("  [GET] http://localhost:8080/courses")
+	fmt.Println("  [POST] http://localhost:8080/courses")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Server Crashed", err)
+	}
+}
