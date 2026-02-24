67fcba79c51661bf329f3ce56de311bb9b63dcc2
Atharva Naik <atharvan.coder@gmail.com>
2026-02-24 15:31:40 +0530

    completed 08 authentication middleware

--- 03_Advanced/08_middleware/main.go
+++ 03_Advanced/08_middleware/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func AuthMidleWare(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+
+		if token != "secret_token" {
+			http.Error(w, "Unauthorized!", http.StatusUnauthorized)
+			return
+		}
+		next(w, r)
+	}
+}
+
+func dashBoardHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the super secret Admin Dashboard!")
+
+}
+func main() {
+	http.HandleFunc("GET /admin", AuthMidleWare(dashBoardHandler))
+	http.ListenAndServe(":8080", nil)
 }
