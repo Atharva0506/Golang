d344d396818f4f981b53305ec7b1a4b2f57b6a08
Atharva Naik <atharvan.coder@gmail.com>
2026-02-24 14:52:37 +0530

    fixed waitgroup deadlock in worker pool and updated slog formatting

--- 03_Advanced/07_concurrency_patterns/main.go
+++ 03_Advanced/07_concurrency_patterns/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"sync"
+	"time"
+)
 
+func emailWorker(workerId int, emailJobs <-chan string, wg *sync.WaitGroup) {
+	// 1. Defer Done() at the top of the function!
+	// This ensures that when the channel closes and the `for` loop finishes, the worker announces it is going home!
+	defer wg.Done()
+
+	for email := range emailJobs {
+		// Used slog.Int instead of string(rune) so it actually prints the number!
+		slog.Info("Worker Processing", slog.Int("WorkerID", workerId), slog.String("Email", email))
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	jobs := make(chan string, 10)
+
+	for i := 1; i <= 3; i++ {
+		// 2. We Add(1) to the WaitGroup BEFORE launching the goroutine!
+		// We are tracking the 3 Workers, not the individual emails!
+		wg.Add(1)
+		go emailWorker(i, jobs, &wg)
+	}
+	emails := []string{"bob@gmail", "alice@gmail", "john@gmail", "sarah@gmail", "mike@gmail", "tom@gmail"}
+	for _, email := range emails {
+		jobs <- email
+	}
+	close(jobs)
+	wg.Wait()
 }
