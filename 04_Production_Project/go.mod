13286043e6de4fd0930948f1a3698a3451d12f99
Atharva Naik <atharvan.coder@gmail.com>
2026-03-02 10:50:53 +0530

    feat:jwt token added

--- 04_Production_Project/go.mod
+++ 04_Production_Project/go.mod
@@ -3,3 +3,5 @@ module github.com/Atharva0506/trading_bot
 go 1.25.0
 
 require github.com/google/uuid v1.6.0
+
+require github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
--- 04_Production_Project/pkg/auth/jwt.go
+++ 04_Production_Project/pkg/auth/jwt.go
@@ -1,7 +1,49 @@
 package auth
 
-// TODO: JWT Authentication Logic
-// Responsibilities:
-// - Generate Cryptographically signed Tokens for Users
-// - Safely validate incoming token signatures using server-side secrets
-// - Expose standard structured claims (Expiration times, Subject ID, Role)
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type Claims struct {
+	UserID    uuid.UUID `json:"user_id"`
+	Role      string    `json:"role"`
+	TokenType string    `json:"token_type"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID uuid.UUID, role, secret, tokenType string, expiration time.Duration) (string, error) {
+	claims := &Claims{
+		UserID:    userID,
+		Role:      role,
+		TokenType: tokenType,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiration)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	payload := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	token, err := payload.SignedString([]byte(secret))
+	if err != nil {
+		return "", err // TDOD ADD stranderd error throuout application
+	}
+	return token, nil
+}
+
+func ValidateToken(tokenString, secret string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, fmt.Errorf("invalid token claims") // or use your apperrors package
+	}
+	return claims, nil
+}
