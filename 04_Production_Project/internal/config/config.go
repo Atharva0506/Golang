fe42ee62d114ec3fb53657f4312bb96f59db4e78
Atharva Naik <atharvan.coder@gmail.com>
2026-03-02 11:43:04 +0530

    feat(config):application  comfig
    - Added APP SERVER JWT LOGGER config

--- 04_Production_Project/internal/config/config.go
+++ 04_Production_Project/internal/config/config.go
@@ -1,7 +1,75 @@
 package config
 
-// TODO: Configuration Management
-// Responsibilities:
-// - Define structs that map to config.yaml and .env variables
-// - Use tools like Viper or envconfig to parse environment properties
-// - Provide a centralized Config object to be injected into the application
+import (
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	App      AppConfig      `yaml:"app"`
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+	JWT      JWTConfig      `yaml:"jwt"`
+	Logger   LoggerConfig   `yaml:"logger"`
+}
+
+type AppConfig struct {
+	Name    string `yaml:"name"`
+	Version string `yaml:"version"`
+}
+
+type ServerConfig struct {
+	Host         string        `yaml:"host"`
+	Port         int           `yaml:"port"`
+	ReadTimeout  time.Duration `yaml:"read_timeout"`
+	WriteTimeout time.Duration `yaml:"write_timeout"`
+}
+
+type DatabaseConfig struct {
+	Host         string        `yaml:"host"`
+	Port         int           `yaml:"port"`
+	User         string        `yaml:"user"`
+	Password     string        `yaml:"password"`
+	Name         string        `yaml:"name"`
+	Driver       string        `yaml:"driver"`
+	MaxOpenConns int           `yaml:"max_open_connections"`
+	MaxIdleConns int           `yaml:"max_idle_connections"`
+	MaxLifetime  time.Duration `yaml:"max_lifetime"`
+}
+
+type JWTConfig struct {
+	Secret        string        `yaml:"secret"`
+	AccessExpiry  time.Duration `yaml:"access_expiry"`
+	RefreshExpiry time.Duration `yaml:"refresh_expiry"`
+}
+
+type LoggerConfig struct {
+	Level  string `yaml:"level"`
+	Format string `yaml:"format"`
+}
+
+func MustLoad() (*Config, error) {
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	var cfg Config
+
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	if dbPassword := os.Getenv("DB_PASSWORD"); dbPassword != "" {
+		cfg.Database.Password = dbPassword
+	}
+
+	if jwtSecret := os.Getenv("JWT_SECRET"); jwtSecret != "" {
+		cfg.JWT.Secret = jwtSecret
+	}
+
+	return &cfg, nil
+
+}
